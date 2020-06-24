bbbc9ef3c5475f5e475daddcca8555d3d6b346d1
Thomas75019 <tlarousse3@gmail.com>
2020-06-24 15:56:31 +0200

    Updates like router

--- routes/like.js
+++ routes/like.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const Like = require('../models/like');
+const likeCtrl = require('../controllers/like');
 
-router.post('/like', '');
-router.get('/likes', '');
-router.get('/like/:id',' ')
-router.del('/like/:id', ' ')
+router.post('/like', likeCtrl.CreateLike);
+router.get('/likes', likeCtrl.GetLikes);
+router.get('/like/:id', likeCtrl.GetOneLike);
+router.del('/like/:id', likeCtrl.DelOneLike);
 
 
 module.exports = router;
